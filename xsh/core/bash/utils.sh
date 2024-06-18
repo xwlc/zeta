619d55a5d7e529cbd20b3830f8d73260c473e545
Charles Wong <charlie-wong@outlook.com>
2024-06-18 11:50:45 +0800

    fix: bash normal var type => scalar

--- xsh/core/bash/utils.sh
+++ xsh/core/bash/utils.sh
@@ -12,13 +12,15 @@ function @zeta:xsh:whats() {
   [[ $# -eq 0 ]] && return
   local var vtype output details
   for var in $@; do
-    vtype='' details=''
+    vtype=''; details=''
     # NOTE 仅显示指定属性的变量
     # => declare -p -x  仅显示[拥有] export 属性变量
     # => declare -p +x  仅显示[没有] export 属性变量
     if declare -p "${var}" >& /dev/null; then
       local attrs="${!var@a}" # 获取属性字符串
-      # TODO [确保]属性值按照 aAilnrtux 顺序排序
+      # NOTE 普通变量属性字符串为空, 用 Z 标记
+      [[ -z "${attrs}" ]] && attrs=Z
+      # NOTE [确保]属性值按照 aAilnrtux 顺序排序
       #      即 help declare 命令帮助中显示的顺序
       # -n 属性表示变量使用时自动间接引用, ${V3} 的值 OK, 等效于 ${!V2}
       #    V1='OK'; V2=V1; declare -n V3=V1
@@ -26,20 +28,20 @@ function @zeta:xsh:whats() {
       # -l 属性表示变量赋值时自动将 大写 -> 小写, 即变量值始终都是[小写]
       # -u 属性表示变量赋值时自动将 大写 <- 小写, 即变量值始终都是[大写]
       local OPTERR  OPTARG  OPTIND=1  opt
-      while getopts 'aAilnrtux' opt "-${attrs}"; do
+      while getopts 'aAilnrtuxZ' opt "-${attrs}"; do
         # 属性显示方式参考 Zsh 手册 14.3.1 节
         # keywords separated by hyphens(-)
         case "${opt}" in
-          a) if [[ -z "${vtype}" ]]; then vtype='array';       else vtype="${vtype}-array";       fi ;;
-          A) if [[ -z "${vtype}" ]]; then vtype='association'; else vtype="${vtype}-association"; fi ;;
-          i) if [[ -z "${vtype}" ]]; then vtype='integer';     else vtype="${vtype}-integer";     fi ;;
-          l) if [[ -z "${vtype}" ]]; then vtype='lower';       else vtype="${vtype}-lower";       fi ;;
-          n) if [[ -z "${vtype}" ]]; then vtype='reference';   else vtype="${vtype}-reference";   fi ;;
-          r) if [[ -z "${vtype}" ]]; then vtype='readonly';    else vtype="${vtype}-readonly";    fi ;;
-          t) if [[ -z "${vtype}" ]]; then vtype='trace';       else vtype="${vtype}-trace";       fi ;;
-          u) if [[ -z "${vtype}" ]]; then vtype='upper';       else vtype="${vtype}-upper";       fi ;;
-          x) if [[ -z "${vtype}" ]]; then vtype='export';      else vtype="${vtype}-export";      fi ;;
-          *) if [[ -z "${vtype}" ]]; then vtype='scalar';      else vtype="${vtype}-scalar";      fi ;;
+          a) vtype='array'        ;;
+          A) vtype='association'  ;;
+          i) vtype='integer'      ;;
+          l) vtype='lower'        ;;
+          n) vtype='reference'    ;;
+          r) vtype='readonly'     ;;
+          t) vtype='trace'        ;;
+          u) vtype='upper'        ;;
+          x) vtype='export'       ;;
+          Z) vtype='scalar'       ;;
         esac
       done
     else
@@ -48,12 +50,9 @@ function @zeta:xsh:whats() {
     fi
 
     if [[ -n "${_SILENT_:-}" ]]; then
-      echo "${vtype}"
-      continue
+      echo "${vtype}"; continue
     else
-      # => `help type`
-      # keyword, builtin, file
-      case "${vtype}" in
+      case "${vtype}" in # 参考 => `help type`
         function) details="$(type ${var} | tail +2)" ;;
         alias) details="$(type ${var} | cut -d'`' -f2 | cut -d"'" -f1)" ;;
       esac
