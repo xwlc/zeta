8751ce31a0cdb55c3464f0ea39452a3e76636e8e
Charles Wong <charlie-wong@outlook.com>
2024-06-27 19:53:12 +0800

    misc: rename and update docs

--- 3rd/favor-tools.sh
+++ 3rd/favor-tools.sh
@@ -113,6 +113,8 @@ if false; then
   sudo apt install ${DRY_RUN} qtbase5-dev
 fi
 
+exit
+
 #######################
 # 下载/解压/编译/配置 #
 #######################
@@ -129,9 +131,52 @@ fi
 # https://github.com/ImageOptim/gifski
 # https://docs.rs/gifski/latest/gifski
 
-# 编程语言
-# https://nodejs.org/dist
-# https://mirrors.ustc.edu.cn/golang
-#
+#################
+# Rust 编程语言 #
+#################
 # https://rust-lang.github.io/rustup/installation/other.html
 # https://forge.rust-lang.org/infra/other-installation-methods.html
+
+# 更新工具链 RUSTUP_DIST_SERVER, 自我更新 RUSTUP_UPDATE_ROOT
+# 软件包下载 ${RUSTUP_DIST_SERVER}/dist/channel-rust-stable.toml
+export RUSTUP_DIST_SERVER="https://static.rust-lang.org"                # 官方
+export RUSTUP_DIST_SERVER="https://mirrors.ustc.edu.cn/rust-static"     # 中科
+export RUSTUP_DIST_SERVER="https://mirrors.tuna.tsinghua.edu.cn/rustup" # 清华
+# 下载 ${RUSTUP_DIST_SERVER}/rustup/dist/<目标平台>/rustup-init
+# 移动 rustup-init -> CARGO_HOME/bin/rustup 添加执行权限
+# 安装工具链 rustup --profile minimal toolchain install stable
+
+# https://rust-lang.github.io/rustup/environment-variables.html
+if [[ -d "${ZETA_DIR}/3rd/vendor/rust/xmeta" ]]; then
+  # 默认值 ~/.rustup 或 %USERPROFILE%/.rustup
+  export RUSTUP_HOME="${ZETA_DIR}/3rd/vendor/rust/xmeta"
+  export RUSTUP_UPDATE_ROOT="${RUSTUP_DIST_SERVER}/rustup"
+fi
+# 清华 https://mirrors.tuna.tsinghua.edu.cn/help/rustup
+# 中科 https://mirrors.ustc.edu.cn/help/rust-static.html
+
+# => ${RUSTUP_DIST_SERVER}/dist/channel-rust-stable.toml
+# => ${RUSTUP_DIST_SERVER}/dist/YYYY-MM-DD/组件-版本-平台.tar.xz
+VERSION=1.79.0; Y4M2D2=2024-06-13; URL="${RUSTUP_DIST_SERVER}/dist/${Y4M2D2}"
+
+# 组件 https://forge.rust-lang.org/infra/channel-layout.html
+# 组件 https://rust-lang.github.io/rustup/concepts/components.html
+# 配置 minimal
+#      rustc            编译器和文档工具
+#      cargo            依赖管理及构建发布
+#      rust-std         标准库组件(交叉编译)
+#      rust-mingw       微软 Win 编译链接支持
+# 配置 default
+#      clippy           风格及质量检测 Lint
+#      rustfmt          Rust 代码格式化工具
+#      rust-docs        https://doc.rust-lang.org/{stable,beta,nightly}
+# 配置 complete
+#      rust-src         支持 rust-analyzer 补全标准库
+#      rust-analyzer    编辑器(IDE)语法解析(VSCode, Vim)
+#                       https://rust-analyzer.github.io
+#                       https://github.com/rust-lang/rust-analyzer
+#      miri             解析中间产物
+COMPONENTS=(rustc  cargo  rust-std  clippy  rustfmt  rust-docs  rust-analyzer)
+for pkg in ${COMPONENTS[@]}; do
+  echo "${URL}/${pkg}-${VERSION}-x86_64-unknown-linux-gnu.tar.xz"
+done
