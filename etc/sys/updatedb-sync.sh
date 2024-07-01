8299cce93817e0004a3eff18779544e2ba5e3663
Charles Wong <charlie-wong@outlook.com>
2024-07-01 10:17:09 +0800

    docs: remove comments

--- etc/sys/updatedb-sync.sh
+++ etc/sys/updatedb-sync.sh
@@ -3,7 +3,6 @@
 THIS_AFP="$(realpath "${0}")"        # 当前文件绝对路径(含名)
 THIS_FNO="$(basename "${THIS_AFP}")" # 仅包含当前文件的文件名
 THIS_DIR="$(dirname  "${THIS_AFP}")" # 当前文件所在的绝对路径
-# printf "[${THIS_AFP}]\n[${THIS_DIR}] [${THIS_FNO}]\n"; exit
 
 sudo cp "${THIS_DIR}/updatedb.conf" /etc/updatedb.conf
 
